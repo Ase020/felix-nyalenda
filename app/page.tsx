3c90791b58c6afc8215a651ea2a98c84301e9a62
felixNyalenda <felixasejunior@gmail.com>
2024-06-13 15:57:58 +0300

    Hero section build

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import { FloatingNavbar } from "@/components/ui/FloatingNavbar";
+import { FaHome } from "react-icons/fa";
 
 export default function Home() {
+  const navItems = [{ name: "Home", link: "/", icon: <FaHome /> }];
+
   return (
-    <main className="">
-      <div className="">Heloo</div>
+    <main className="bg-black-100 relative flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="text-white max-w-7xl w-full">
+        <FloatingNavbar navItems={navItems} />
+        <Hero />
+      </div>
     </main>
   );
 }
